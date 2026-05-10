1e4bca6211458ae0ad0387099801f2073fee04ea
Kelvinkk2 <kelvinkarikaribusinesskk2@gmail.com>
2026-05-10 17:52:29 +0100

    fix double api in baseurl

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://flexirents.com/api',
+  baseURL: 'https://flexirents.com',
 })
 
 api.interceptors.request.use((config) => {
